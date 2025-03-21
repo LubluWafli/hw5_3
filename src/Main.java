0d972b5110316fd0d123891c046774e45f478ac6
Akame style <eiljo@mail.ru>
2025-03-21 03:52:38 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
+    Scanner scan = new Scanner(System.in);
 
+    String input = scan.nextLine();
         try
         {
-            checkNumber("111");
+            checkNumber(input);
         }
         catch(Exception e)
         {
